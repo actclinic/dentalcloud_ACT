bb83266648187ad2c8bc1738c0c77c5df1d404d6
MinThutaSawNaing <minthutasawnaing@yahoo.com>
2026-04-02 16:27:40 +0630

    Fix expenses search icon alignment

--- components/ExpensesView.tsx
+++ components/ExpensesView.tsx
@@ -200,9 +200,11 @@ const ExpensesView: React.FC<ExpensesViewProps> = ({
               }}
               className="pl-10 pr-4 py-2 border border-gray-200 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 w-full sm:w-64"
             />
-            <svg className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
+            <span className="pointer-events-none absolute inset-y-0 left-3 flex items-center">
+              <svg className="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </span>
           </div>
           <select
             value={categoryFilter}
